6ac2585638aa0d6b73b97fc9d342eb5e4e282522
Dima Smirnov <dim.smir@inbox.ru>
2022-12-09 15:37:02 +0600

    init

---
+++ App.tsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import {
+  SafeAreaView
+} from 'react-native';
+
+
+
+const App = () => {
+ 
+
+  return (
+    <SafeAreaView >
+      Hello
+    </SafeAreaView>
+  );
+};
+
+
+
+export default App;
