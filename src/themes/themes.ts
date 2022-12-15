799b914617355cd0718cba5512eb325a2029fd37
Dima Smirnov <dim.smir@inbox.ru>
2022-12-15 09:53:24 +0600

    add themes

---
+++ src/themes/themes.ts
@@ -0,0 +1,19 @@
+export const font = {
+  fontFamily: 'Roboto',
+  fontWeight: 'regular',
+};
+
+export const themes = {
+  font: { ...font },
+  colors: {
+    primaryText: '#c1c1c1',
+    secondaryText: '#767676',
+    errorBorder: '#F92749',
+    defaultBorder: '#949599',
+    primaryBackground: '#1F4037',
+    secondBackground: '#023320',
+    buttonPrimary: '#00A86C',
+    linkTextColor: '#51B5D4',
+    rippleColor: 'rgba(46, 211, 152, 0.8)',
+  },
+};
