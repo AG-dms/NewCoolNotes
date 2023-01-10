d509ae84e5ab564cc0176a38e16390ae92f33fd7
Dima Smirnov <dim.smir@inbox.ru>
2023-01-10 10:42:32 +0600

    add common text editor variables

---
+++ src/utils/common.ts
@@ -0,0 +1,14 @@
+export const textColors = [
+  '#00AB89FF',
+  '#CC1400FF',
+  '#9EB3FFFF',
+  '#0089ABFF',
+  '#7F15BDFF',
+  'black',
+  'white',
+];
+
+export const backGroundColors = ['#00BD61FF', '#BD3E4DFF', '#E6EB1AFF', '#0B67BDFF', '#65008CFF'];
+
+// 1=  10px, 2 = 13px, 3 = 16px, 4 = 18px, 5 = 24px, 6 = 32px, 7 = 48px;
+export const fontSize = [];
